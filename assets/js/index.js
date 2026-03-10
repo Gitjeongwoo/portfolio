f8e362c466fd52bdd11534e0012e220b8d3d347d
jw970625 <parkjeongwoo970625@gmail.com>
2026-03-10 19:53:39 +0900

    btn
    btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,9 +35,9 @@ $(document).ready(function () {
 /* project_sec */
 gsap.registerPlugin(ScrollTrigger);
 
-const mm = gsap.matchMedia();
+const mob = gsap.matchMedia();
 
-mm.add("(min-width: 769px)", () => {
+mob.add("(min-width: 540px)", () => {
   const sections = gsap.utils.toArray('#project_sec .sec');
 
   // 패널 고정
@@ -125,3 +125,46 @@ gsap.utils.toArray('#list_sec .list_wrap a.list').forEach((elem) => {
   });
 });
 
+$(document).ready(function(){
+
+  let totalNum = $('.swiper-slide a').length;
+  let pageNum = 0;
+
+  //각 목록을 클릭했을때 해당내용 나옴
+  $(".swiper-slide a").click(function(){
+    pageNum = $(".swiper-slide a").index(this);
+    $(".page-num span:nth-child(1)").text(pageNum+1); //현재 페이지번호
+    $(".modal-content li").eq(pageNum).stop().fadeIn(); //클릭한 해당목록의 내용
+    $(".modal").stop().fadeIn(); //검정배경
+    $(".modal").scrollTop(0); //새로 창을 열때는 항상 맨위로 이동하여 보이게 함
+  });
+
+  //이전다음버튼
+  $(".pre").click(function(){
+    $(".modal").scrollTop(0);
+    if(pageNum > 0){
+      $(".modal-content li").eq(pageNum).hide(); 
+      pageNum--;
+      $(".page-num span:nth-child(1)").text(pageNum+1); 
+      $(".modal-content li").eq(pageNum).stop().fadeIn();
+    };
+  });
+  $(".next").click(function(){
+    $(".modal").scrollTop(0);
+    if(pageNum < totalNum-1){
+      $(".modal-content li").eq(pageNum).hide(); 
+      pageNum++;
+      $(".page-num span:nth-child(1)").text(pageNum+1); 
+      $(".modal-content li").eq(pageNum).stop().fadeIn();
+    };
+  });
+
+  //닫기버튼
+  //모달창에 컨텐츠가 포함이 되어 있기때문에 모달부분을 클릭할때도 닫히게 하면 컨텐츠를 클릭해도 담힘
+  // $(".close, .modal").click(function(){
+  $(".close").click(function(){
+    $(".modal").stop().fadeOut();
+		$(".modal-content li").stop().fadeOut();
+  });
+
+});
